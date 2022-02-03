8012c05d8297ad8ad4f624252809bc08d0d4e653
mian-ali <mian41935@gmail.com>
2022-02-03 17:35:31 +0500

    Prepare for deploment

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; AmazonaShop</Col> 
         </Row>
       </Container>
     </footer>
