6000f29923f49a08e19253bd2f8572e746cc99d9
Ali Ahmad <mian41935@gmail.com>
2022-12-04 15:51:48 +0500

    Build Created for  production move heroku to Cyclic.sh

---
+++ frontend/build/precache-manifest.2e673748ebb5a80290f6a2ee6ddb7ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff0f59d862213a6d98e08d17f8fba2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c139e7f372832d356f",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c139e7f372832d356f",
+    "url": "/static/js/main.21db30dd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
